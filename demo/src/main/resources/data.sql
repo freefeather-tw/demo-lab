a734acded9352fd1e00900c8dd39dfd7508b0156
Jack Yang <jack.yang@webcomm.com.tw>
2023-09-13 16:19:03 +0800

    新增ELK相關設定

--- demo/src/main/resources/data.sql
+++ demo/src/main/resources/data.sql
@@ -1,2 +1,2 @@
-insert into users (id, name, age) values (random_uuid(), 'Jack', 42);
-insert into users (id, name, age) values (random_uuid(), 'Mary', 35);
+insert into users (id, name, age) values ('f09bf0a5-7da6-47a7-b904-d90d436dbec0', 'Jack', 42);
+insert into users (id, name, age) values ('6fdfd583-1fb9-4b1a-aa94-0cdc562f8fda', 'Mary', 35);
